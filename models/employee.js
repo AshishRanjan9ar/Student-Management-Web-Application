174dc32be6b73d49c2e58057e735947013d19711
Ashish Ranjan <ashishranjan9ar@gmail.com>
2023-07-06 03:13:59 +0530

    Delete models directory

--- models/employee.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const employeeSchema = new mongoose.Schema({
-    employee_email: { type: String, required: true },
-    employee_password:{type:String,require:true},
-    employee_name: { type: String, required: true }
-},{timestamps:true});
-const Employee = mongoose.model('Employee', employeeSchema);
-module.exports = Employee;
